5b99490e1d1031886ed85e3305b617dbf1070d3d
belcogo <bel.cogo27@gmail.com>
2026-06-16 19:01:28 -0300

    fix: path

--- src/TrabalhoGB/main.cpp
+++ src/TrabalhoGB/main.cpp
@@ -176,7 +176,7 @@ int main()
 	glfwInit();
 
 	// Criação da janela GLFW
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Trabalho GA - Bel Cogo e Bruno Hoffmann", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Trabalho GB - Bel Cogo e Bruno Hoffmann", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Fazendo o registro da função de callback para a janela GLFW
